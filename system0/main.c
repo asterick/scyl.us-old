993160f360a4ed9c84c75e9871e14767f18ee26a
Bryon Vandiver <unicdk@gmail.com>
2017-06-04 16:36:08 -0700

    Checkpoint

--- system0/main.c
+++ system0/main.c
@@ -1,5 +1,7 @@
+//extern const unsigned int _DATA_START;
+volatile unsigned int b;
+
 int main(void) {
 	volatile unsigned int* a = (volatile unsigned int*)0xB0000000;
-	for (;;) (*a)++;
+	for (;;) { *a = (*a)++; b++; }
 }
-
