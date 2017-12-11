d7b3f0eca8175249629a7bc29f0c985ebd17b767
Bryon Vandiver <unicdk@gmail.com>
2017-12-11 15:31:19 -0800

    Checkpoint

--- core/include/imports.h
+++ core/include/imports.h
@@ -1,18 +1,7 @@
 #pragma once
 
-void invalidate(uint32_t physical, uint32_t logical);
-
+void execute(uint32_t, uint32_t);
 uint32_t exception(uint32_t code, uint32_t pc, uint32_t delayed, uint32_t cop);
 uint32_t read(uint32_t address, uint32_t code, uint32_t pc, uint32_t delayed);
 uint32_t write(uint32_t address, uint32_t value, uint32_t mask, uint32_t pc, uint32_t delayed);
-
-void execute(uint32_t, uint32_t);
-
-// COP0 functions
-uint32_t mfc0(uint32_t, uint32_t, uint32_t);
-uint32_t mtc0(uint32_t, uint32_t, uint32_t, uint32_t);
-uint32_t rfe(uint32_t, uint32_t);
-uint32_t tlbr(uint32_t, uint32_t);
-uint32_t tlbwi(uint32_t, uint32_t);
-uint32_t tlbwr(uint32_t, uint32_t);
-uint32_t tlbp(uint32_t, uint32_t);
+void invalidate(uint32_t physical, uint32_t logical);
