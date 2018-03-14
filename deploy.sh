0b2514c9ba1a7c8ab2601486762610451e417cc8
Bryon Vandiver <unicdk@gmail.com>
2018-03-14 14:31:26 -0700

    Cleanup

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-npm run core
-ssh node@scyl.us "source .profile; pm2 stop scyl.us"
-ssh node@scyl.us "source .profile; cd ~/scyl.us; git reset --hard"
-ssh node@scyl.us "source .profile; cd ~/scyl.us; git pull"
-ssh node@scyl.us "source .profile; cd ~/scyl.us; npm prune; npm install"
-ssh node@scyl.us "source .profile; cd ~/scyl.us; npm run build"
-scp -r assets/*.wasm node@scyl.us:~/scyl.us/assets
-ssh node@scyl.us "source .profile; pm2 start scyl.us"
