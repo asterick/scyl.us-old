5ec16910f16cf14eab2e66ecfd62dc71f7ebbf1a
Bryon Vandiver <unicdk@gmail.com>
2018-03-16 17:53:58 -0700

    Move setup to where it should be

--- system0/startup.s
+++ system0/startup.s
@@ -3,11 +3,25 @@
 	.align		4
 	.globl		main
 	.globl		_STACK_TOP
+	.globl		_DATA_ROM
+	.globl		_DATA_START
+	.globl		_DATA_SIZE
+	.globl		memcpy
 
 _reset:
+	# Setup stack pointer
 	la 	$sp, _STACK_TOP
+
+	# Setup initialized ram sections
+	la	$a0, _DATA_START
+	la	$a1, _DATA_ROM
+	la	$a2, _DATA_SIZE
+	jal	memcpy
+
+	# Jump to main
 	jal main
-	j   _reset
+	
+	j   .
 
 	# TLB Exception handler
 	.section	.tlb, "ax", @progbits
