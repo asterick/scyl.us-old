d8e6710e63c42af378b0be01ac5d3b3765e3ef67
Bryon Vandiver <unicdk@gmail.com>
2018-03-20 18:14:06 -0700

    DMA works now

--- core/dma/dma.cc
+++ core/dma/dma.cc
@@ -12,7 +12,7 @@
 static const int MAX_CHANNELS = 8;
 
 enum DMATrigger {
-   TRIGGER_NONE = 0,
+   TRIGGER_NONE,
    TRIGGER_DMA0,
    TRIGGER_DMA1,
    TRIGGER_DMA2,
@@ -76,6 +76,28 @@ static inline uint32_t adjust(const int shift, const uint32_t value) {
    }
 }
 
+static bool check_trigger(int channel) {
+   switch (channel) {
+   case TRIGGER_NONE: return true;
+   case TRIGGER_DMA0: return channels[0].flags.active == 0;
+   case TRIGGER_DMA1: return channels[1].flags.active == 0;
+   case TRIGGER_DMA2: return channels[2].flags.active == 0;
+   case TRIGGER_DMA3: return channels[3].flags.active == 0;
+   case TRIGGER_DMA4: return channels[4].flags.active == 0;
+   case TRIGGER_DMA5: return channels[5].flags.active == 0;
+   case TRIGGER_DMA6: return channels[6].flags.active == 0;
+   case TRIGGER_DMA7: return channels[7].flags.active == 0;
+
+   // Unimplemented
+   case TRIGGER_GPU_TX_FIFO:
+   case TRIGGER_GPU_RX_FIFO:
+   case TRIGGER_DSP_IDLE:
+   case TRIGGER_DSP_FINISHED:
+   default:
+      return false;
+   }
+}
+
 EXPORT void dma_advance() {
    if (!active) return ;
 
@@ -91,22 +113,19 @@ EXPORT void dma_advance() {
       // TODO: FOR NO TRIGGER, RAM/ROM COPIES
       // THIS SHOULD USE A FAST ENGINE INSTEAD
 
-      for (;;) {
-         // TODO: CHECK TRIGGER
-
+      while (check_trigger(channel.flags.trigger)) {
          uint32_t source = lookup(channel.source, false, exception);
          uint32_t value = read(source, exception);
          uint32_t target = lookup(channel.target, true, exception);
 
-         registers.clocks --; // This always uses the cross bus
+         value = adjust((target & 3) - (source & 3), value);
+         registers.clocks -= 2; // No cross bar
 
          switch (channel.flags.copy_width) {
             case WIDTH_BIT8:
-               value = adjust((target & 3) - (source & 3), value);
                write(target, value, 0xFF << ((source & 3) * 8), exception);
                break ;
             case WIDTH_BIT16:
-               value = adjust((target & 2) - (source & 2), value);
                write(target, value, (target & 2) ? 0xFFFF0000 : 0x0000FFFF, exception);
                break ;
             case WIDTH_BIT32:
