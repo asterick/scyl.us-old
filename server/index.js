83d49fad4e12579508b91dc09548df3bf74cd773
Bryon Vandiver <unicdk@gmail.com>
2018-07-19 14:18:47 -0700

    Starting google login integration

--- server/index.js
+++ server/index.js
@@ -6,6 +6,16 @@ const Config = require("../config.json");
 // === Main ===
 const app = express();
 
+app.engine('html', require('ejs').renderFile);
+
+console.log(app.get('views'));
+
+app.get("/", (req, res) => {
+	res.render('index.html', {
+		client_id: Config.oauth.client_id
+	});
+});
+
 if (Config.environment === 'development') {
     const webpack = require("webpack");
     const compiler = webpack(require("../webpack.config.js"));
@@ -18,6 +28,8 @@ if (Config.environment === 'development') {
 
 	app.use(express.static('assets'));
 }
+
+// "asdf{asdf}".replace(/\{(.*?)\}/ig, (_, a) => (console.log(k, a), "!!!"))
 
 app.listen(Config.server.port, () => {
     logging("debug", `Server started on port ${Config.server.port}`);
